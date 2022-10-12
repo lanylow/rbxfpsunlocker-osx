d26ba472fcd33b24727cff2fc228faddaaa10cfd
lanylow <lanylow@protonmail.com>
2022-10-12 17:00:44 +0200

    Move main_module initialization to the roblox_process constructor.

--- inc/process/roblox_player.hpp
+++ inc/process/roblox_player.hpp
@@ -3,10 +3,6 @@
 namespace process {
   class roblox_player : public process::roblox_process {
   public:
-    explicit roblox_player() : process::roblox_process("RobloxPlayer") {
-      auto module = get_module("/RobloxPlayer");
-      if (!module.has_value()) throw std::runtime_error("Couldn't get the main module");
-      main_module = module.value();
-    }
+    explicit roblox_player() : process::roblox_process("RobloxPlayer", "/RobloxPlayer") { }
   };
 }
--- inc/process/roblox_studio.hpp
+++ inc/process/roblox_studio.hpp
@@ -3,10 +3,6 @@
 namespace process {
   class roblox_studio : public process::roblox_process {
   public:
-    explicit roblox_studio() : process::roblox_process("RobloxStudio") {
-      auto module = get_module("/RobloxStudio");
-      if (!module.has_value()) throw std::runtime_error("Couldn't get the main module");
-      main_module = module.value();
-    }
+    explicit roblox_studio() : process::roblox_process("RobloxStudio", "/RobloxStudio") { }
   };
 }
